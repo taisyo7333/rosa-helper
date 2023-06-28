a39019916b8094371250b1b93fb98c09530d87f0
Daisuke Inoue <4737631+taisyo7333@users.noreply.github.com>
2023-06-29 07:39:43 +0900

    bugfix - can not delete operator-role and oidc-provider

--- scripts/rosa-delete-cluster.sh
+++ scripts/rosa-delete-cluster.sh
@@ -2,7 +2,7 @@
 # https://docs.openshift.com/rosa/rosa_install_access_delete_clusters/rosa_getting_started_iam/rosa-deleting-cluster.html
 
 CLUSTER_NAME=dinoue
-CLUSTER_ID=$(rosa describe cluster -c="${CLUSTER_NAME}" -o json | jq .id)
+CLUSTER_ID=$(rosa describe cluster -c="${CLUSTER_NAME}" -o json | jq .id | tr -d '"')
 
 # Enter the following command to delete a cluster and watch the logs, replacing <cluster_name> with the name or ID of your cluster:
 rosa delete cluster --cluster="${CLUSTER_NAME}" --yes --watch

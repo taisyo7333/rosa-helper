409fb2c2669c75c4063be2af74bcdc506e6114cd
Daisuke Inoue <4737631+taisyo7333@users.noreply.github.com>
2023-10-23 10:43:43 +0900

    remove aro scripts

--- aro-helper/scripts/aro-delete-cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-# https://learn.microsoft.com/ja-jp/azure/openshift/tutorial-delete-cluster
-CLUSTER=dinoue
-RESOURCEGROUP=dinoue-aro
-
-az login
-az aro delete --resource-group $RESOURCEGROUP --name $CLUSTER
-

a630b43cdb4e5e0f529c09908e3fa8cf1df25778
Daisuke Inoue <4737631+taisyo7333@users.noreply.github.com>
2024-02-02 20:55:11 +0900

    use rosa login

--- scripts/rosa-provision-auto-vpc.sh
+++ scripts/rosa-provision-auto-vpc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -xe
 
 CLUSTER_NAME=${1:?Must provide cluster name for you}
-CLUSTER_REGION="ap-northeast-2"
-CLUSTER_VERSION=${2:?Must provide OCP version such as 4.13.6}
-
+CLUSTER_REGION="ap-northeast-1"
+CLUSTER_VERSION=${2:?Must provide OCP version such as 4.13.8}
+# --region us-east-1 -
 # if Existing the above cluster , do not proccedd, stop here with error status
 
 # Prerequisite
@@ -12,15 +12,14 @@ aws iam set-security-token-service-preferences --global-endpoint-token-version v
 # Provisioning ROSA
 rosa version
 
+rosa login
+
 # verify
 # it's for non-sts
 # rosa verify permissions --region="${CLUSTER_REGION}"
 rosa verify quota  --region="${CLUSTER_REGION}"
 rosa verify oc
 rosa init
-
-# rosa login
-#rosa login --token="${ROSA_TOKEN}" --region="${CLUSTER_REGION}"
 
 rosa create account-roles --mode auto --yes
 
@@ -40,4 +39,8 @@ rosa describe cluster -c "${CLUSTER_NAME}"
 # watch
 rosa logs install -c "${CLUSTER_NAME}" --watch
 
+# create cluster-admin user
 rosa create admin --cluster="${CLUSTER_NAME}"
+
+# testing purpose: Running the network verification manually using the CLI
+# rosa verify network -c "${CLUSTER_NAME}"
